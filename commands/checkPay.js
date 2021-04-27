d0f95aaecd726720ddded72228860272dd01e525
olliez-mods <socksinthewash@gmail.com>
2021-04-27 19:28:37 -0400

    added hours to pay checker

--- commands/checkPay.js
+++ commands/checkPay.js
@@ -5,11 +5,11 @@ exports.run = async () => {
     totalPay = 0;
     table.forEach(e => {
         if(e.hours <= 0){
-          console.log(`Date: ${e.month} ${e.date} (${e.weekDay}), Pay: 0 (did no work)`);
+          console.log(`Date: ${e.month} ${e.date} (${e.weekDay}), hours: ${e.hours}, Pay: 0 (did no work)`);
         }else{
             let pay = (e.hours*config.pay)-(config.pay*config.bufferForPay);
             totalPay += pay;
-        console.log(`Date: ${e.month} ${e.date} (${e.weekDay}), Pay: ${pay}`);
+        console.log(`Date: ${e.month} ${e.date} (${e.weekDay}), hours: ${e.hours}, Pay: ${pay}`);
       }
     });
     console.log(`\n\n\nTOTAL PAY: ${totalPay}`);
